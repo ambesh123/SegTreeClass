c36fc7a94f20f2a685c97d922780ef92b446ae23
Ambesh Tiwari <35375583+ambesh123@users.noreply.github.com>
2019-11-26 00:11:59 +0530

    Making Segment Tree Generic
    Added template support so that the segment tree can be of any class.
    Also added a separate combine() function so that segment tree can act as sum segtree, RMQ segtree and what not... :)

--- SegTreeClass.cpp
+++ SegTreeClass.cpp
@@ -1,34 +1,51 @@
 
-class SegTree{  // sum Segment Tree
+/*
+Generic Segment Tree
+
+Just Change 2 things according to requirement:
+1. combine function ( a+b for sum segment tree, max(a,b) for RMQ query etc.)
+2. ZERO value  (The value which you consider zero for combine function eg. INT_MAX for Min RMQ segment Tree and 0 for Sum Segment Tree)
+
+    -- Author : Ambesh Tiwari
+*/
+
+template<typename T>
+class SegTree{  //generic segment Tree
     private:
 
-        long *tree;
+        T *tree;
         int N;
+        int ZERO; // Zero value for query
+
+        T combine(T x, T y){    //define the combine function here
+            return max(x,y);
+        }
 
         void build(){
             for(int i = N-1; i > 0; i--){
-                tree[i] = tree[2*i] + tree[2*i+1];
+                tree[i] = combine(tree[i<<1] , tree[i<<1|1]);
             }
         }
 
     public:
 
-        SegTree(int arr[], int n){
-            tree = new long[2*n];
+        SegTree(T arr[], int n){
+            ZERO = 0; // define zero value here
+            tree = new T[2*n];
             for(int i = 0; i < n; i++)tree[n+i] = arr[i];
             N = n;
             build();
         }
 
-        long query(int l, int r){
+        T query(int l, int r){
             l += N;
             r += N;
 
-            long ret = 0;
+            T ret = ZERO;
 
             while(l <= r){
-                if(l&1)ret += tree[l++];
-                if(!(r&1))ret += tree[r--];
+                if(l&1)ret = combine(ret, tree[l++]);
+                if(!(r&1))ret = combine(ret, tree[r--]);
                 l >>= 1;
                 r >>= 1;
             }
@@ -36,11 +53,12 @@ class SegTree{  // sum Segment Tree
             return ret;
         }
 
-        void pointUpdate(int pos, int val){
+        void pointUpdate(int pos, T val){
             pos += N;
-            while(pos ){
-                tree[pos] += val;
+            tree[pos] = val;
+            while(pos > 1){
                 pos >>= 1;
+                tree[pos] = combine(tree[pos<<1], tree[pos<<1|1]);
             }
         }
 
